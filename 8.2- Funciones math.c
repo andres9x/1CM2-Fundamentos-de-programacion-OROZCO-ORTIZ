2117be6274e1b5dc9d753be97dc475d57b4ccdb1
andres9x <72401170+andres9x@users.noreply.github.com>
2020-11-30 11:08:27 -0600

    Create 8.2- Funciones math.c

---
+++ 8.2- Funciones math.c
@@ -0,0 +1,18 @@
+/*Funcion Exponencial*/
+
+#include <stdio.h>
+#include <stdlib.h>  /*Tenemos las "DIRECTIVAS DE PROCESAMIENTO" */
+#include <math.h>
+
+int main(int argc, char** argv) {       /*Función pincipal*/
+    double x = 45;
+    printf("Funcion Exponencial");
+    printf("\n \n Ejemplo 1:");
+    printf("\n %.2f",exp(1.0)); 
+    
+    printf("\n Ejemplo 2:");
+    printf("\n %.2f",exp((1.0)*(2.0))+exp(5)); 
+    
+    printf("\n Jesus Andres Orozco Ortiz");
+    
+}
