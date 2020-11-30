78e75afbcd32f1ec7d2e7584d6c3e24da4bb3451
andres9x <72401170+andres9x@users.noreply.github.com>
2020-11-30 11:22:02 -0600

    Create 8.4 - Funciones math.c

---
+++ 8.4 - Funciones math.c
@@ -0,0 +1,20 @@
+/*Funcion Logaritmo de x en base 10*/
+
+#include <stdio.h>
+#include <stdlib.h>  /*Tenemos las "DIRECTIVAS DE PROCESAMIENTO" */
+#include <math.h>
+
+int main(int argc, char** argv) {       /*Función pincipal*/
+    double x = 45;
+    printf("Funcion Logaritmo de x en base 10");
+    printf("\n \n Ejemplo 1:");
+    printf("\n %.2f",log10(10)); 
+    
+    printf("\n Ejemplo 2:");
+    printf("\n %.2f",log10((10)*(5))+log(100)); 
+    
+    printf("\n \n Jesus Andres Orozco Ortiz");
+    
+}
+
+    
