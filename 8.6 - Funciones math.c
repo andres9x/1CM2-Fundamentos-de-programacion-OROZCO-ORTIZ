b411572be50963253257fe4531231f21caf0c4d1
andres9x <72401170+andres9x@users.noreply.github.com>
2020-11-30 11:34:55 -0600

    Create 8.6 - Funciones math.c

---
+++ 8.6 - Funciones math.c
@@ -0,0 +1,18 @@
+/*Funcion que Redondea a "x" al entero mas pequeño que no sea menor que "x" */
+
+#include <stdio.h>
+#include <stdlib.h>  /*Tenemos las "DIRECTIVAS DE PROCESAMIENTO" */
+#include <math.h>
+
+int main(int argc, char** argv) {       /*Función pincipal*/
+    double x = 45;
+    printf("Funcion que Redondea a x al entero mas pequeño que no sea menor que x");
+    printf("\n \n Ejemplo 1:");
+    printf("\n %.2f",ceil(8.2541)); 
+    
+    printf("\n Ejemplo 2:");
+    printf("\n %.2f",ceil((2.1)+.021*(-.020102))); 
+    
+    printf("\n \n Jesus Andres Orozco Ortiz");
+    
+}
