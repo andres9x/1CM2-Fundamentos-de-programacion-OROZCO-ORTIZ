7eccfe996920dab034e01b8f525d71a4c396eb7c
andres9x <72401170+andres9x@users.noreply.github.com>
2020-11-30 10:59:47 -0600

    Create 8.1- Funciones math.c

---
+++ 8.1- Funciones math.c
@@ -0,0 +1,12 @@
+/*Función de raiz cuadrada*/
+
+#include <stdio.h>
+#include <stdlib.h>  /*Tenemos las "DIRECTIVAS DE PROCESAMIENTO" */
+#include <math.h>
+
+int main(int argc, char** argv) {       /*Función pincipal*/
+    double x = 45;
+    printf("Raiz cuadrada");
+    printf("\n %.2f",sqrt(81)); 
+    
+}
