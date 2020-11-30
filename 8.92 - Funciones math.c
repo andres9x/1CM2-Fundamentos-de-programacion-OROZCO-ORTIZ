5b62e7b43f890532673c27dc36936ee94a1d8646
andres9x <72401170+andres9x@users.noreply.github.com>
2020-11-30 12:00:17 -0600

    Create 8.92 - Funciones math.c

---
+++ 8.92 - Funciones math.c
@@ -0,0 +1,19 @@
+/*Funcion coseno trigonométrico de x (x en numeros hexagesimales)*/
+
+#include <stdio.h>
+#include <stdlib.h>  /*Tenemos las "DIRECTIVAS DE PROCESAMIENTO" */
+#include <math.h>
+
+int main(int argc, char** argv) {       /*Función pincipal*/
+    double x = 45;
+    printf("Funcion coseno trigonométrico de x (x en numeros hexagesimales)");
+    printf("\n \n Ejemplo 1:");
+    printf("\n %.2f",cos(180)); 
+    
+    printf("\n Ejemplo 2:");
+    printf("\n %.2f",cos(1)); 
+  
+  
+    printf("\n \n Jesus Andres Orozco Ortiz");
+    
+}
