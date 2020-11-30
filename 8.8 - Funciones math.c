191f84b5b1cd96a1b8907e3bcb2f2ee470798a93
andres9x <72401170+andres9x@users.noreply.github.com>
2020-11-30 11:46:37 -0600

    Create 8.8 - Funciones math.c

---
+++ 8.8 - Funciones math.c
@@ -0,0 +1,21 @@
+/*Funcion x elevado a la potencia y (x^) */
+
+#include <stdio.h>
+#include <stdlib.h>  /*Tenemos las "DIRECTIVAS DE PROCESAMIENTO" */
+#include <math.h>
+
+int main(int argc, char** argv) {       /*Función pincipal*/
+    double x = 45;
+    printf("Funcion x elevado a la potencia y (x^)");
+    printf("\n \n Ejemplo 1:");
+    printf("\n %.2f",pow(2, 4)); 
+    
+    printf("\n Ejemplo 2:");
+    printf("\n %.2f",pow(3, 2)); 
+    
+    printf("\n Ejemplo 3:");
+    printf("\n %.2f",pow(2, 5)); 
+    
+    printf("\n \n Jesus Andres Orozco Ortiz");
+    
+}
