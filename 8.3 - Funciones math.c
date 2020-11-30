a94c1ef2bfe12ba4b939bdc3f3f966f290b46cc2
andres9x <72401170+andres9x@users.noreply.github.com>
2020-11-30 11:14:01 -0600

    Create 8.3 - Funciones math.c

---
+++ 8.3 - Funciones math.c
@@ -0,0 +1,18 @@
+/*Funcion Logaritmo Natural*/
+
+#include <stdio.h>
+#include <stdlib.h>  /*Tenemos las "DIRECTIVAS DE PROCESAMIENTO" */
+#include <math.h>
+
+int main(int argc, char** argv) {       /*Función pincipal*/
+    double x = 45;
+    printf("Funcion Logaritmo Natural");
+    printf("\n \n Ejemplo 1:");
+    printf("\n %.2f",log(2.718282)); 
+    
+    printf("\n Ejemplo 2:");
+    printf("\n %.2f",log((2.718282)*(2.718282))+log(5846.2581)); 
+    
+    printf("\n \n Jesus Andres Orozco Ortiz");
+    
+}
