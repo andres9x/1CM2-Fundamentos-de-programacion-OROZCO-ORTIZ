a1f905551d183b8d59bd1cba22714f682ceac19e
andres9x <72401170+andres9x@users.noreply.github.com>
2021-01-13 01:36:34 -0600

    Create 10.1.2.-TareaArreglosDeCaracteresArray2.c

---
+++ 10.1.2.-TareaArreglosDeCaracteresArray2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+char C1[4];   /* Donde "C1" es la cadena (o "Array")*/
+
+C1[0]='h'; C1[1]='o'; C1[2]='l'; C1[3]='a';
+
+printf("La palabra en la  cadena es: %c%c%c%c \n", C1[0],C1[1],C1[2],C1[3]);
+printf("\nJesús Andrés Orozco Ortiz");
+return 0;
+}
